ff9ea00037d40053a886dcb7412c4da37d4f3e35
Yash Kadam <iyashkadam7@gmail.com>
2026-04-02 11:40:21 +0530

    course.java changes

--- Codeforge/src/main/java/com/codeforge/models/Course.java
+++ Codeforge/src/main/java/com/codeforge/models/Course.java
@@ -1,9 +1,15 @@
 package com.codeforge.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
 
     @Id
@@ -14,40 +20,40 @@ public class Course {
     private String description;
     private int duration;
 
-    public Course() {
-    }
-
-    public Course(String title, String description, int duration) {
-        this.title = title;
-        this.description = description;
-        this.duration = duration;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
+//    public Course() {
+//    }
+//
+//    public Course(String title, String description, int duration) {
+//        this.title = title;
+//        this.description = description;
+//        this.duration = duration;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public int getDuration() {
+//        return duration;
+//    }
+//
+//    public void setDuration(int duration) {
+//        this.duration = duration;
+//    }
 }
